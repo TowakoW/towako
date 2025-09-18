91c9da71cfd0eea04d704829f02d511132f7c6d4
Towako Wunsch <towakowunsch@berkeley.edu>
2025-09-17 18:07:15 -0700

    done with hw2

---
+++ homework2/homework2.py
@@ -0,0 +1,78 @@
+# File: homework2.py
+
+# Your file path should look like:
+# python_decal_fa25/yourname/homework2/homework2.py
+
+# Questions (Answer these in the homework2.py file as comments):
+
+# 1) What’s the difference between Git, GitHub, and Git Bash?
+# Git is the overall software company, while GitHub is the website for 
+# organization as well as file sharing. Git Bash is the terminal 
+#alternate for editing.
+
+# 2) What’s the difference between the terminal and the command line?
+# terminal and command line are about the same but one is for mac and 
+#the other is for windows. 
+# 3) How does Windows PowerShell differ from Git Bash?
+# different commands
+# 4) What’s the difference between Anaconda, conda, and Python?
+# python is a coding language and is a part of the andaconda essential 
+#packages. Conda is also on this package.
+
+# 5) What is VS Code? 
+#a code editor that can be downloaded
+
+# 6) What is a Jupyter Notebook? How is it different from Jupyter Lab?
+# jupyter lab is a web based interface, and notebook is a part of 
+#the package.
+
+# 7) What does ~/ mean?
+# shortcut to reach home directory
+
+# 8) What’s the difference between an absolute path and a relative path?
+# location of files, one is overall and the other is relative to working 
+#directory.
+
+# 9) Imagine you're in your "yourname" repo. Write the absolute and relative paths to "course_assignments/homework2".
+# c:/Users/towa/python_decal_fa25/course_assignments/homework2
+# python_decal_fa25/course_assignments/homework2
+
+# 10) What command lets you move from "course_assignments/homework2/" to "course_assignments/"?
+# cd ..
+
+# 11) What would rm ./ do in your current directory? (Don’t try it!)
+# rm will remove the file.
+
+# 12) What do the following commands do?
+# git add - adds to remote directory
+# git commit - commits to remote directory
+# git push - sends to remote directory
+
+# 13) What's the difference between "git add ." and "git add <file>"?
+# git add . will add all files not added. specifying a file will only add that file.
+
+# 14) What do "git status" and "git log -1" do?
+# git status shows any files not added/commited/pushed to remote repo. git log -l will display committed 
+#snapshots
+
+# 15) What’s the difference between cloning a repository and pulling from it?
+# cloning will make a copy, pulling will directly edit the original file.
+
+# 16) What has been your most frustrating bug or error in this class so far? How did you troubleshoot or fix it?
+# I kept getting "deprecated" is not defined during lecture on VSCode. Not quite sure how I troubleshooted, but I 
+#isolated and then ran the debugger.
+
+# 17) What’s a question you still have? What’s something you’re confused about?
+# no particular questions, just bewildered.
+# I now edit this, the terminal output is...messed up. works fine if i specifically run 
+# the code below, but shows a syntax error for '&' when run normally. makes me angry.
+# in the process of trying to use the vscode built in ai to fix.
+# not quite sure what I did but hope it's legible...runs if i use "python run and debug" but will not run normaly on python output.
+
+# 18) Tell me a fun fact!
+# I danced professionaly for a year in Oklahoma City.
+
+# 19) Print your favorite math expression you've learned in Python so far. 
+# (Hint: Use print() and add a comment explaining what it does.)
+print('cd')
+# moves between files.
