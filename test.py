f7bfc209f47e7b7b4325735a045d3b196233e12d
Towako Wunsch <towakowunsch@berkeley.edu>
2025-10-06 22:06:00 -0700

    save test.py

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 #File: test.py
-print(5+5)
+# print(5+5)
 
+# def printprac(word):
+#     for i in range(len(word)):
+#         print(word[i])
+
+# printprac('Python')
+
+# printprac('Astronomy')
+
+# def print_char(word):
+#     for i in range(len(str(word))):
+#         print(word[i])
+# print_char('hello')
+
+# nums = []
